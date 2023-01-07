dfa80261db9c06e3f14692beee869f6c99790591
Addison Severnak <addisonsevernak@gmail.com>
2023-01-07 16:57:45 -0600

    initial form structure complete

--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -1,8 +1,59 @@
 import React from 'react';
 
 const Form =  (props) => {
+    const { change, submit } = props;
+    const { username, email, password, checked } = props.values;
+
+    const onChange = evt => {
+        const { name, value, checked, type } = evt.target;
+        const newVal = type === 'checkbox' ? checked : value;
+        change(name, newVal);
+    }
+
+    const onSubmit = evt => {
+        evt.prevetnDefault();
+        submit();
+    }
+
     return (
-        <h1>A Form!</h1>
+        <div>
+            <h1>A Form!</h1>
+            <form onSubmit={onSubmit}>
+                <label>Name:
+                    <input
+                        type='text'
+                        name='username'
+                        value={username}
+                        onChange={onChange}
+                    />
+                <label>Email:
+                </label>
+                    <input
+                        type='email'
+                        name='email'
+                        value={email}
+                        onChange={onChange}
+                    />
+                </label>
+                <label>Password:
+                    <input
+                        type='password'
+                        name='password'
+                        value={password}
+                        onChange={onChange}
+                    />
+                </label>
+                <label>Terms of Service
+                    <input
+                        type='checkbox'
+                        name='tos'
+                        checked={checked}
+                        onChange={onChange}
+                    />
+                </label>
+                <input type='submit' value='Create a user!' />
+            </form>
+        </div>
     )
 }
 
